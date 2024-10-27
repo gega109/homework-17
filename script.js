6dafb16900f444efdf934e5c7242e85bd4a4fc64
gega109 <bakuravar1@gmail.com>
2024-10-27 14:11:05 +0400

    homework 17

---
+++ script.js
@@ -0,0 +1,104 @@
+// hm 1
+// num = (a,b) => {
+//     return (a+b)
+// }
+// let lesson = num(15,25)
+// console.log(lesson);
+
+// hm2
+// const box = (function (a, b) {
+//   return a - b;
+// })(10, 5);
+// console.log(box);
+
+// hm3
+// const isEven = (num) => num % 2 === 0;
+// iseven = (num) => {
+//   num % 2 === 0;
+// };
+// console.log(isEven(4));
+
+// hm4
+
+// function hm4(a, b, callback) {
+//   return callback(a, b);
+// }
+// function multiplied(a, b) {
+//   return a * b;
+// }
+// console.log(hm4(10, 5, multiplied));
+
+// hm5
+// let positivnum = (num) => num > 0;
+// console.log(positivnum(4));
+
+// hm6
+// let box = function (a) {
+//     return (a * 2)
+// }
+// console.log(box(10));
+
+// hm7
+// function numfun(a, callback) {
+//   return callback(a);
+// }
+// function multiplied(a) {
+//   return a * 2;
+// }
+// console.log(numfun(5, multiplied));
+
+// hm8
+
+// let box = (num) => num % 3 === 0;
+// console.log(box(3));
+
+// hm9
+// function hm9(x, callback) {
+//   return callback(x);
+// }
+// function iseven(x) {
+//   return x % 2 === 0;
+// }
+// console.log(hm9(10, iseven));
+
+// hm10
+// let cube = (num) => num * num * num;
+// console.log(cube(4));
+
+// hm11
+// function hm11(a, b, callback) {
+//   return callback(a, b);
+// }
+// function multiplied(a, b) {
+//   return a * b;
+// }
+// console.log(hm11(10, 5, multiplied));
+
+// hm12
+// let hm12 = (num) => num > 0;
+// console.log(hm12(11));
+
+// hm13
+// let anon = function (x) {
+//   return x / 2;
+// };
+// console.log(anon(5));
+
+// hm14
+// function hm14(a, b, callback) {
+//   return callback(a, b);
+// }
+// function plus(a, b) {
+//   return a + b;
+// }
+// function deduction(a, b) {
+//   return a - b;
+// }
+// function multiplied(a, b) {
+//   return a * b;
+// }
+// console.log(hm14 (15,20,plus));
+
+// hm15
+// let hm15 = (num) => num * num;
+// console.log(hm15 (5));
